cee793ffd89cf59b8c0d6d7a6cac58312e7237a7
Jolomi <Jolomi@example.com>
2021-02-25 19:55:26 +0000

    Modified ImageViewFragment.kt to take higher quality pictures so as to improve text recognition accuracy. Bugs: On initial install of the app on a devices, when a user tries to load the camera, the app crashes but ask for user permission, but when it is loaded a second time it works fine from then onwards. will proceed with the project at this time and address this bug later.

--- app/src/main/java/com/example/budgettracker/camera/ImageViewFragment.kt
+++ app/src/main/java/com/example/budgettracker/camera/ImageViewFragment.kt
@@ -6,19 +6,26 @@ import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 import com.example.budgettracker.R
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.text.TextRecognition
 import kotlinx.android.synthetic.main.fragment_imageview.*
+import java.io.File
 
+private const val FILE_NAME = "image.jpg"
 private const val REQUEST_CODE = 10
+private lateinit var imageFile: File
 class ImageViewFragment : Fragment(R.layout.fragment_imageview) {
 
     private lateinit var safeContext: Context
@@ -42,7 +49,8 @@ class ImageViewFragment : Fragment(R.layout.fragment_imageview) {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         //check if user has successfully taken a picture with the camera
         if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK){
-            val takenImage = data?.extras?.get("data") as Bitmap
+           // val takenImage = data?.extras?.get("data") as Bitmap
+            val takenImage = BitmapFactory.decodeFile(imageFile.absolutePath)
             imageView.setImageBitmap(takenImage)
             textRecognition(takenImage)
         }else{
@@ -54,13 +62,23 @@ class ImageViewFragment : Fragment(R.layout.fragment_imageview) {
     private fun takePicture(){
 
         val takePicture = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        imageFile = getImageFile(FILE_NAME)
         //check for a camera
+        val fileProvider = FileProvider.getUriForFile(safeContext, "com.example.fileprovider", imageFile)
+        takePicture.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider)
         //if (takePicture.resolveActivity(this.packageManager) != null) {
         startActivityForResult(takePicture, REQUEST_CODE)
         //} else {
         // Toast.makeText(this, "No camera detected!", Toast.LENGTH_LONG).show()
         //}
     }
+
+    private fun getImageFile(fileName: String): File {
+        //access package-specific directories
+        val storageDirectory = safeContext.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(fileName, ".jpg", storageDirectory)
+    }
+
 
     private fun textRecognition(image: Bitmap) {
 
@@ -71,15 +89,14 @@ class ImageViewFragment : Fragment(R.layout.fragment_imageview) {
             .addOnSuccessListener { visionText ->
                 for (block in visionText.textBlocks){
                     val blockText = block.text
-                    Toast.makeText(safeContext,blockText, Toast.LENGTH_LONG).show()
+                    //Log.i("ImageViewFragment",blockText)
                     for (line in block.lines) {
                         val lineText = line.text
-                        Toast.makeText(safeContext,lineText, Toast.LENGTH_LONG).show()
+                        Log.i("ImageViewFragment",lineText)
                     }
                 }
             }
             .addOnFailureListener { e ->
-
             }
     }
 
